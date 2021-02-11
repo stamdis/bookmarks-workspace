033bfaee9307702567e31bea8212ab24a3a87541
Stamatis Stamatiadis <stamatiadis.s@gmail.com>
2021-02-11 09:49:22 +0200

    Update feature entity interface for bookmarks
    Assignment notes: objective 2 (each bookmark should have the following properties: id, name, url, group)

--- libs/bookmarks-lib/src/lib/+state/bookmarks.models.ts
+++ libs/bookmarks-lib/src/lib/+state/bookmarks.models.ts
@@ -3,4 +3,7 @@
  */
 export interface BookmarksEntity {
   id: string | number; // Primary ID
+  name: string; // The name of the bookmark
+  url: string; // The URL of the bookmark
+  group: string; // The group in which the bookmark belongs (work, leisure, personal, etc.)
 }
