bbabd9908410481d3d4b345dd395604ef7d6ebe1
Stamatis Stamatiadis <stamatiadis.s@gmail.com>
2021-02-11 20:39:04 +0200

    Install angular flex layout library
    Command: npm i -s @angular/flex-layout

--- libs/bookmarks-ui/src/lib/bookmarks-ui.module.ts
+++ libs/bookmarks-ui/src/lib/bookmarks-ui.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AngularMaterialModule } from '@bookmarks-workspace/angular-material';
 import { BookmarksOverviewComponent } from './bookmarks-overview/bookmarks-overview.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BookmarksLibModule } from '@bookmarks-workspace/bookmarks-lib';
 import { BookmarksFormComponent } from './bookmarks-form/bookmarks-form.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
   imports: [
@@ -15,7 +16,9 @@ import { BookmarksFormComponent } from './bookmarks-form/bookmarks-form.componen
     CommonModule,
     AngularMaterialModule,
     FormsModule,
-    BookmarksLibModule
+    ReactiveFormsModule,
+    BookmarksLibModule,
+    FlexLayoutModule
   ],
   declarations: [BookmarksOverviewComponent, BookmarksFormComponent],
   exports: [BookmarksOverviewComponent, BookmarksFormComponent],
